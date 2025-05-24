d87839c88f81d4e529fac7ac8984dcc00beea87f
Justin Thomas <git@justi.in>
2025-05-24 20:00:14 +0530

    refactor(fish): move functions to individual files
    - Move rkit functions from config.fish to individual files in ~/.config/fish/functions/
    - Create separate files for clone, cdc, edit functions
    - Add dedicated completions file
    - Improve modularity and maintainability
    - Follow Fish shell best practices for function organization

--- install.fish
+++ install.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 # Script to install rkit shell functions
-# This script adds useful functions to your fish configuration file
+# This script adds useful functions to your fish functions directory
 # for working with rkit repositories.
 
 # Check if rkit is installed
@@ -20,57 +20,43 @@ if not command -v fzf > /dev/null
     exit 1
 end
 
-# Function to add to fish config
-function add_to_fish_config
-    set config_file "$HOME/.config/fish/config.fish"
+# Create functions directory if it doesn't exist
+set functions_dir "$HOME/.config/fish/functions"
+if not test -d "$functions_dir"
+    mkdir -p "$functions_dir"
+    echo "Created functions directory at $functions_dir"
+end
 
-    # Create backup of config file
-    if test -f "$config_file"
-        cp "$config_file" "$config_file.bak"
-        echo "Created backup of $config_file at $config_file.bak"
+# Function to create individual function files
+function create_function_file
+    set function_name $argv[1]
+    set function_content $argv[2]
+    set function_file "$functions_dir/$function_name.fish"
+
+    # Check if function file already exists
+    if test -f "$function_file"
+        echo "$function_name function is already installed at $function_file"
+        return
     end
 
-    # Initialize the functions string
-    set functions_to_add ""
+    echo "The following function will be created at $function_file:"
+    echo
+    echo "$function_content"
+    echo
+    read -l -P "Do you want to install the $function_name function? (y/N) " reply
+    if test "$reply" = "y" -o "$reply" = "Y"
+        echo "$function_content" > "$function_file"
+        echo "Created $function_name function at $function_file"
+    end
+end
 
-    # Check and ask for clone function
-    if not grep -q "^function clone" "$config_file"
-        echo "The following function will be added to $config_file:"
-        echo
-        echo "function clone"
-        echo "    rkit clone \$argv"
-        echo "end"
-        echo
-        read -l -P "Do you want to install the clone function? (y/N) " reply
-        if test "$reply" = "y" -o "$reply" = "Y"
-            set functions_to_add "$functions_to_add
-
-function clone
+# Create clone function
+create_function_file "clone" "function clone
     rkit clone \$argv
 end"
-        end
-    else
-        echo "clone function is already installed in $config_file"
-    end
 
-    # Check and ask for cdc function
-    if not grep -q "^function cdc" "$config_file"
-        echo "The following function will be added to $config_file:"
-        echo
-        echo "function cdc"
-        echo "    set -l query \$argv[1]"
-        echo "    if test -n \"\$query\""
-        echo "        cd (rkit ls | fzf --preview 'rkit view {}' --query \"\$query\")"
-        echo "    else"
-        echo "        cd (rkit ls | fzf --preview 'rkit view {}')"
-        echo "    end"
-        echo "end"
-        echo
-        read -l -P "Do you want to install the cdc function? (y/N) " reply
-        if test "$reply" = "y" -o "$reply" = "Y"
-            set functions_to_add "$functions_to_add
-
-function cdc
+# Create cdc function
+create_function_file "cdc" "function cdc
     set -l query \$argv[1]
     if test -n \"\$query\"
         cd (rkit ls | fzf --preview 'rkit view {}' --query \"\$query\")
@@ -78,29 +64,9 @@ function cdc
         cd (rkit ls | fzf --preview 'rkit view {}')
     end
 end"
-        end
-    else
-        echo "cdc function is already installed in $config_file"
-    end
 
-    # Check and ask for edit function
-    if not grep -q "^function edit" "$config_file"
-        echo "The following function will be added to $config_file:"
-        echo
-        echo "function edit"
-        echo "    set -l query \$argv[1]"
-        echo "    if test -n \"\$query\""
-        echo "        code (rkit ls | fzf --preview 'rkit view {}' --query \"\$query\")"
-        echo "    else"
-        echo "        code (rkit ls | fzf --preview 'rkit view {}')"
-        echo "    end"
-        echo "end"
-        echo
-        read -l -P "Do you want to install the edit function? (y/N) " reply
-        if test "$reply" = "y" -o "$reply" = "Y"
-            set functions_to_add "$functions_to_add
-
-function edit
+# Create edit function
+create_function_file "edit" "function edit
     set -l query \$argv[1]
     if test -n \"\$query\"
         code (rkit ls | fzf --preview 'rkit view {}' --query \"\$query\")
@@ -108,44 +74,26 @@ function edit
         code (rkit ls | fzf --preview 'rkit view {}')
     end
 end"
-        end
-    else
-        echo "edit function is already installed in $config_file"
-    end
 
-    # Add completion configuration
-    if not grep -q "complete -c edit" "$config_file"
-        echo "The following completion configuration will be added to $config_file:"
-        echo
-        echo "# rkit completions"
-        echo "complete -c edit -a '(rkit ls)'"
-        echo "complete -c cdc -a '(rkit ls)'"
-        echo
-        read -l -P "Do you want to install the completion configuration? (y/N) " reply
-        if test "$reply" = "y" -o "$reply" = "Y"
-            set functions_to_add "$functions_to_add
-
-# rkit completions
+# Create completions file
+set completions_file "$functions_dir/rkit-completions.fish"
+if not test -f "$completions_file"
+    echo "The following completions will be created at $completions_file:"
+    echo
+    echo "# rkit completions"
+    echo "complete -c edit -a '(rkit ls)'"
+    echo "complete -c cdc -a '(rkit ls)'"
+    echo
+    read -l -P "Do you want to install the completions? (y/N) " reply
+    if test "$reply" = "y" -o "$reply" = "Y"
+        echo "# rkit completions
 complete -c edit -a '(rkit ls)'
-complete -c cdc -a '(rkit ls)'"
-        end
-    else
-        echo "Completion configuration is already installed in $config_file"
+complete -c cdc -a '(rkit ls)'" > "$completions_file"
+        echo "Created completions at $completions_file"
     end
-
-    # If no functions were selected, exit
-    if test -z "$functions_to_add"
-        echo "No new functions were selected for installation."
-        return
-    end
-
-    # Add the selected functions to the config file
-    echo "# rkit functions$functions_to_add" >> "$config_file"
-
-    echo "Added selected functions to $config_file"
-    echo "Please restart your shell or run 'source $config_file' to use the new functions"
+else
+    echo "Completions are already installed at $completions_file"
 end
 
-# Main installation
-echo "Installing rkit functions for fish..."
-add_to_fish_config 
+echo "Installation complete!"
+echo "Please restart your shell or run 'source $HOME/.config/fish/config.fish' to use the new functions" 
